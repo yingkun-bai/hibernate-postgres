bb03390e28168a5f20231c58dbb5934d22a61af6
Yingkun and Coding <baiyingkun@gmail.com>
2025-02-06 10:50:23 +0100

    Update BatchingTest.java

--- demo/src/test/java/app/BatchingTest.java
+++ demo/src/test/java/app/BatchingTest.java
@@ -1,5 +1,9 @@
 package app;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+
 import app.config.AppConfig;
 import jakarta.persistence.GenerationType;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,23 +37,26 @@ class BatchingTest {
 		sut.createEntitiesInBatchSize(400, batchSize);
 	}
 
-	@ParameterizedTest
+	@ParameterizedTest(name = "{index}: Writing {0} rows in batches of {1}")
 	@CsvSource({
-			"400, 25",
-			"400, 50",
-			"400, 100",
-			"400, 200",
-			"400, 400",
-			"400000, 25",
-			"400000, 50",
-			"400000, 100",
-			"400000, 200",
-			"400000, 400",
-			"400000,800",
+			"400, 1, (Smallest dataset without batching)",
+			"400, 25, (Small dataset, small batch size)",
+			"400, 50, (Small dataset, moderate batching)",
+			"400, 100, (Small dataset, larger batch size)",
+			"400, 200, (Small dataset, near full batch)",
+			"400, 400, (Small dataset, full batch processing)",
+
+			"400000, 1, (Large dataset without batching, worst case)",
+			"400000, 50, (Large dataset, small batch size)",
+			"400000, 100, (Large dataset, moderate batching)",
+			"400000, 200, (Large dataset, increasing batch size)",
+			"400000, 400, (Large dataset, testing a higher batch size)",
+			"400000, 800, (Large dataset, testing an even larger batch size)"
 	})
 	void compare_total_size_and_batch_size(int totalSize, int batchSize) {
 		sut.createEntitiesInBatchSize(totalSize, batchSize);
 	}
+
 
 
 	@ParameterizedTest
@@ -61,5 +68,15 @@ class BatchingTest {
 	@Test
 	void will_not_batch_when_mixed_statements() {
 		sut.createMixedEntities(40, 10);
+	}
+
+	@Test
+	void small_batch_size_profile() {
+		sut.createEntitiesInBatchSize(400_000, 25);
+	}
+
+	@Test
+	void bit_batch_size_profile() {
+		sut.createEntitiesInBatchSize(400_000, 800);
 	}
 }
